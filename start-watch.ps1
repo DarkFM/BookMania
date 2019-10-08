c3fbf1fb9dc97d05e936f24722c1c513375b2f8e
Clinton <c.bestmann@hotmail.com>
2019-10-07 21:49:46 -0700

    Added script for dev workflow

---
+++ start-watch.ps1
@@ -0,0 +1,39 @@
+﻿
+$GulpJob = {
+    gulp --gulpfile ".\wwwroot\gulpfile.js" watch
+}
+
+$DotnetWatch = {
+    dotnet watch run
+}
+
+# https://stackoverflow.com/a/17388991
+function start-jobhere([scriptblock]$block) {
+    return Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -Script $block
+}
+
+$dotnetwatchJOB = start-jobhere($DotnetWatch);
+Start-Sleep -s 10
+$gulpJOB = start-jobhere($GulpJob);
+
+Write-Host "BrowserLink and Gulp Sass started"
+Write-Host "Dotnet Watch started"
+Write-Host "Press Ctrl + C to terminate..."
+
+# wait till terminating command Ctrl+C is issued
+# https://powershell.org/forums/topic/wait-till-ctrl-c/
+[console]::TreatControlCAsInput = $true
+while ($true)
+{
+    if ([console]::KeyAvailable)
+    {
+        $key = [system.console]::readkey($true)
+
+        if (($key.modifiers -band [consolemodifiers]"control") -and ($key.key -eq "C"))
+        {
+            "Terminating..."
+            Stop-Job -Id $dotnetwatchJOB.Id, $gulpJOB.Id
+            break
+        }
+    }
+}
